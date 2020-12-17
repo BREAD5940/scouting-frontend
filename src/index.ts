13ba29511041b99998825290c21b727673592caa
Annika <alarson23@dtechhs.org>
2020-12-17 14:41:57 -0800

    Add basic Express functionality

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@
  * @author Annika
  */
 
+import express from 'express';
 import {ConfigLoader} from './config';
 import {JSONBackend, SQLBackend, InfiniteRecharge, StorageBackend} from 'frc-scouting';
 
 const CONFIG_PATH = `${__dirname}/../config.json`;
 const SQLITE_REGEX = /\.(db|sql(ite3?)?)$/;
-
 
 declare global {
     var Config: ConfigLoader;
@@ -19,7 +19,10 @@ declare global {
 global.Config = new ConfigLoader(CONFIG_PATH);
 
 if (!Config.storageLocation) {
-    throw new Error(`You must specify a "storageLocation" in the configuration file (${CONFIG_PATH}).`);
+    throw new Error(`You must specify "storageLocation" in the configuration file (${CONFIG_PATH}).`);
+}
+if (!Config.port) {
+    throw new Error(`You must specify "port" in the configuration file (${CONFIG_PATH}).`);
 }
 
 if (SQLITE_REGEX.test(Config.storageLocation)) {
@@ -27,3 +30,12 @@ if (SQLITE_REGEX.test(Config.storageLocation)) {
 } else { // JSON time
     global.Backend = new JSONBackend(Config.storageLocation, new InfiniteRecharge.InfiniteRechargeJSON());
 }
+
+const server = express();
+
+// TODO: do stuff
+// authentication
+// views
+// idk
+
+server.listen(Config.port, () => console.log(`Listening on http://127.0.0.1:${Config.port}`));
