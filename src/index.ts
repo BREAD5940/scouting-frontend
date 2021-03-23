33bd32f89d5dd0c3ecf4e4bc6f4394557c7e35db
KrishKittur <73321517+KrishKittur@users.noreply.github.com>
2021-03-23 10:54:00 -0700

    Fix things

--- src/index.ts
+++ src/index.ts
@@ -128,19 +128,18 @@ server.get('/', async (req: AuthenticatedRequest, res) => {
         </head>
         <body><div class="logged-in-content">
           <div class="text-group">
-            Hello! This is the FRC Team 5940 scouting website.
-            <p>You are logged in as Krish Kittur`;
-      html += `
+            Hello! This is the FRC Team ${Config.teamNumber || 5940} scouting website.
+            <p>You are logged in as You are logged in as ${(req.oidc.user as any).name
             <a href="/logout">Log out</a></p>
           </div>
           <ul class="buttons">
-            <li><a href="/addteam"><div class="button">Add Team</div></a></li>
-            <li><a href="/addmatch"><div class="button">Add Match</div></a></li>
-            <li><a href="/viewteam"><div class="button">View Team</div></a></li>
-            <li><a href="/viewmatch"><div class="button">View Match</div></a></li>
-          </ul>
-        </div></body>
-      </html>`;
+              <li><a href="/addteam"><div class="button">Add Team</div></a></li>
+              <li><a href="/addmatch"><div class="button">Add Match</div></a></li>
+              <li><a href="/viewteam"><div class="button">View Team</div></a></li>
+               <li><a href="/viewmatch"><div class="button">View Match</div></a></li>
+         </ul>
+         </div></body>
+       </html>`;
     } else {
         html += `
         <html>
